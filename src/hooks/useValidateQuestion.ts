31a14450e442a959bf6d0da0938f4fb4fd505d1e
anne-hyeyeon <khy2106@naver.com>
2025-01-20 03:19:36 +0900

    fix: 금칙어 환경 변수로 관리

--- src/hooks/useValidateQuestion.ts
+++ src/hooks/useValidateQuestion.ts
@@ -1,10 +1,10 @@
-import { BAD_WORDS } from "../constants/restricted-words";
-
 export const useValidateQuestion = () => {
   const validateQuestion = (question: string) => {
     const lowercaseQuestion = question.toLowerCase();
+    const restrictedWords =
+      import.meta.env.VITE_RESTRICTED_WORDS?.split(",") || [];
 
-    const hasBadWord = BAD_WORDS.some((word: string) =>
+    const hasBadWord = restrictedWords.some((word: string) =>
       lowercaseQuestion.includes(word.toLowerCase())
     );
 
